877e4d94dcf29a0df369d678afe32ed9da2ea9a0
rafante2@gmail.com <rafante2@gmail.com>
2020-09-13 17:05:29 -0300

    initial commit with standard assets

---
+++ Assets/Propulsor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Propulsor : MonoBehaviour
+{
+    public float force;
+    public float maxForce;
+    public Rigidbody body;
+
+    // Start is called before the first frame update
+    void FixedUpdate()
+    {
+        body.AddForce(new Vector3(0, -Physics.gravity.y * body.mass + .5f, 0), ForceMode.Force);
+    }
+}
